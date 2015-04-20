6e99da50c997457e96a25c95b09972c47392f860
Erwan Bousse <erwan.bousse@irisa.fr>
2015-04-20 17:25:04 +0200

    Forgor to add a new file containing strings for the plaink3 events generator.

---
+++ fr.inria.diverse.trace.plaink3.tracematerialextractor/src/fr/inria/diverse/trace/plaink3/tracematerialextractor/Plaink3MaterialStrings.java
@@ -0,0 +1,13 @@
+package fr.inria.diverse.trace.plaink3.tracematerialextractor;
+
+import org.eclipse.emf.ecore.EClass;
+
+public class Plaink3MaterialStrings {
+
+	public static final String fillEventSuffix =  "_FillEvent";
+	
+	public static String fillEventClassName(EClass macroEventClass) {
+		return macroEventClass.getName() + fillEventSuffix;
+	}
+	
+}
