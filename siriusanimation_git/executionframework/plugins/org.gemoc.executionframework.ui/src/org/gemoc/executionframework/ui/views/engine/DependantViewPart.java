7698ee068f428d770afca19ea955ed4bf479b528
Didier Vojtisek <didier.vojtisek@inria.fr>
2015-08-20 16:45:54 +0200

    moving DependantViewPart from engine.io to executionframework.ui

---
+++ siriusanimation_git/executionframework/plugins/org.gemoc.executionframework.ui/src/org/gemoc/executionframework/ui/views/engine/DependantViewPart.java
@@ -0,0 +1,44 @@
+package org.gemoc.executionframework.ui.views.engine;
+
+import org.eclipse.ui.IViewSite;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.part.ViewPart;
+import org.gemoc.commons.eclipse.ui.ViewHelper;
+
+public abstract class DependantViewPart extends ViewPart implements
+		IEngineSelectionListener {
+
+	
+	public DependantViewPart()
+	{
+	}
+	
+	@Override
+	public void init(IViewSite site) throws PartInitException {
+		super.init(site);
+		startListeningToMotorSelectionChange();
+	}
+	
+	@Override
+	public void dispose() {
+		super.dispose();
+		stopListeningToMotorSelectionChange();
+	}
+
+	private EnginesStatusView _enginesStatusView;
+
+	private void startListeningToMotorSelectionChange() {
+		_enginesStatusView = ViewHelper.retrieveView(EnginesStatusView.ID);
+		if (_enginesStatusView != null) {
+			_enginesStatusView.addEngineSelectionListener(this);
+		}
+	}
+
+	private void stopListeningToMotorSelectionChange() {
+		if (_enginesStatusView != null) {
+			_enginesStatusView.removeEngineSelectionListener(this);
+		}
+	}
+
+
+}
