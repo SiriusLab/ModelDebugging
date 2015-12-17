498537e6ecdd3dfd364d70627c6d3eb0ec4fb9ba
Didier Vojtisek <didier.vojtisek@inria.fr>
2015-12-17 21:45:32 +0100

    [refactoring] renaming org.gemoc.gemoc_language_workbench.api into org.gemoc.xdsmlframework.api

--- framework/framework_commons/plugins/org.gemoc.gemoc_language_workbench.api/src/org/gemoc/gemoc_language_workbench/api/Activator.java
+++ framework/framework_commons/plugins/org.gemoc.gemoc_language_workbench.api/src/org/gemoc/gemoc_language_workbench/api/Activator.java
@@ -11,7 +11,7 @@ public class Activator extends Plugin
 
 	
 	// The plug-in ID
-	public static final String PLUGIN_ID = "org.gemoc.gemoc_language_workbench.api"; //$NON-NLS-1$
+	public static final String PLUGIN_ID = "org.gemoc.xdsmlframework.api"; //$NON-NLS-1$
 
 	// The shared instance
 	private static Activator plugin;
