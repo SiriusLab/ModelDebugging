eb8c474c2cfa4512bcf0e09684e4e3408a12c87a
Fabien Coulon <fabien.coulon@inria.fr>
2016-01-14 15:21:53 +0100

    [Generator Trace Addon] Fix call graph Take in account: * order of calls * lamba expressions

--- trace/generator/plugins/fr.inria.diverse.trace.gemoc.generator/src/fr/inria/diverse/trace/gemoc/generator/CallHierarchyHelper.java
+++ trace/generator/plugins/fr.inria.diverse.trace.gemoc.generator/src/fr/inria/diverse/trace/gemoc/generator/CallHierarchyHelper.java
@@ -7,10 +7,15 @@ import org.eclipse.jdt.core.IJavaElement;
 import org.eclipse.jdt.core.IMember;
 import org.eclipse.jdt.core.IMethod;
 import org.eclipse.jdt.internal.corext.callhierarchy.CallHierarchy;
+import org.eclipse.jdt.internal.corext.callhierarchy.CallLocation;
+import org.eclipse.jdt.internal.corext.callhierarchy.MethodCall;
 import org.eclipse.jdt.internal.corext.callhierarchy.MethodWrapper;
 
 public class CallHierarchyHelper {
 
+	/**
+	 * Find methods calling method 'm'
+	 */
 	public static HashSet<IMethod> getCallersOf(IMethod m) {
 
 	    CallHierarchy callHierarchy = CallHierarchy.getDefault();
@@ -22,6 +27,25 @@ public class CallHierarchyHelper {
 	    for (MethodWrapper mw : methodWrappers) {
 	        MethodWrapper[] mw2 = mw.getCalls(new NullProgressMonitor());
 	        HashSet<IMethod> temp = getIMethods(mw2);
+	        callers.addAll(temp);
+	    }
+
+	    return callers;
+	}
+	
+	/**
+	 * Find calling sites for method 'm' 
+	 */
+	public static HashSet<CallLocation> getCallLocationsOf(IMethod m) {
+		CallHierarchy callHierarchy = CallHierarchy.getDefault();
+
+	    IMember[] members = { m };
+
+	    MethodWrapper[] methodWrappers = callHierarchy.getCallerRoots(members);
+	    HashSet<CallLocation> callers = new HashSet<CallLocation>();
+	    for (MethodWrapper mw : methodWrappers) {
+	        MethodWrapper[] mw2 = mw.getCalls(new NullProgressMonitor());
+	        HashSet<CallLocation> temp = getCallLocations(mw2);
 	        callers.addAll(temp);
 	    }
 
@@ -49,6 +73,21 @@ public class CallHierarchyHelper {
 			e.printStackTrace();
 		}
 		return null;
+	}
+	
+	private static HashSet<CallLocation> getCallLocations(MethodWrapper[] methodWrappers) {
+	    HashSet<CallLocation> c = new HashSet<CallLocation>();
+	    for (MethodWrapper m : methodWrappers) {
+	    	CallLocation im = getCallLocation(m);
+	        if (im != null) {
+	            c.add(im);
+	        }
+	    }
+	    return c;
+	}
+	
+	private static CallLocation getCallLocation(MethodWrapper m) {
+		return m.getMethodCall().getFirstCallLocation();
 	}
 	
 }
