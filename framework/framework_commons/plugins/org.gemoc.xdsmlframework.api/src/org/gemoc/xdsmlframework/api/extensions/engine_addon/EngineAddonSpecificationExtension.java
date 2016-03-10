e92842e9835de7557e054fba09cdacc7c7dd993b
Didier Vojtisek <didier.vojtisek@inria.fr>
2016-03-10 15:30:30 +0100

    added a field in addon definition in order to indicate to eclipse which view(s) should be opened if the addon is used (will allow to remove the "open view" addon)

--- framework/framework_commons/plugins/org.gemoc.xdsmlframework.api/src/org/gemoc/xdsmlframework/api/extensions/engine_addon/EngineAddonSpecificationExtension.java
+++ framework/framework_commons/plugins/org.gemoc.xdsmlframework.api/src/org/gemoc/xdsmlframework/api/extensions/engine_addon/EngineAddonSpecificationExtension.java
@@ -1,5 +1,8 @@
 package org.gemoc.xdsmlframework.api.extensions.engine_addon;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.eclipse.core.runtime.CoreException;
 import org.gemoc.xdsmlframework.api.engine_addon.IEngineAddon;
 import org.gemoc.xdsmlframework.api.extensions.Extension;
@@ -25,6 +28,22 @@ public class EngineAddonSpecificationExtension extends Extension
 	public String getAddonGroupId()
 	{
 		return getAttribute(EngineAddonSpecificationExtensionPoint.GEMOC_ENGINE_ADDON_EXTENSION_POINT_ADDONGROUPID);
+	}
+	
+	/**
+	 * 
+	 * @return the list of ids of the views that should be opened when using the addon
+	 */
+	public List<String> getOpenViewIds()
+	{
+		String viewIds = getAttribute(EngineAddonSpecificationExtensionPoint.GEMOC_ENGINE_ADDON_EXTENSION_POINT_OPENVIEWIDS);
+		ArrayList<String> idsList = new ArrayList<String>();
+		if(viewIds != null){
+			for(String s : viewIds.split(",")){
+				idsList.add(s.trim());
+			}
+		}
+		return idsList;
 	}
 	
 	public IEngineAddon instanciateComponent() throws CoreException
