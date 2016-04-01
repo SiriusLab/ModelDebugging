0789270fa1963f169740fec61e9be0e28b5c1fd1
Erwan Bousse <ebousse@users.noreply.github.com>
2016-04-01 18:00:56 +0200

    Add defaults in IGemocTraceManager to keep compiling old trace managers

--- trace/commons/plugins/fr.inria.diverse.trace.gemoc.api/src/fr/inria/diverse/trace/gemoc/api/IGemocTraceManager.java
+++ trace/commons/plugins/fr.inria.diverse.trace.gemoc.api/src/fr/inria/diverse/trace/gemoc/api/IGemocTraceManager.java
@@ -19,8 +19,20 @@ import fr.inria.diverse.trace.api.ITraceManager;
 public interface IGemocTraceManager extends ITraceManager {
 
 	boolean addStep(org.gemoc.executionframework.engine.mse.MSEOccurrence mseOccurrence);
+
+	default void addState() {
+		throw new IllegalStateException(
+				"The old addState method should not be used with new trace managers! Regenerate a trace manager.");
+	}
+
+	default boolean addStateIfChanged() {
+		throw new IllegalStateException(
+				"The old addState method should not be used with new trace managers! Regenerate a trace manager.");
+	}
+
 	default void addState(Set<ModelChange> modelChanges) {
 		// For now we do nothing by default, so that former trace managers can keep compiling
-		throw new IllegalStateException("The new addState method should not be used with old trace managers! Disable the new add state, or regenerate a trace manager.");
+		throw new IllegalStateException(
+				"The new addState method should not be used with old trace managers! Regenerate a trace manager.");
 	};
 }
