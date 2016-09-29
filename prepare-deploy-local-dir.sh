588ecd83d59914162ba4b2dd87a1c4cc5bb0db9b
Yvan Lussaud <yvan.lussaud@obeo.fr>
2016-09-29 12:03:50 +0200

    Fixed deploy script

--- prepare-deploy-local-dir.sh
+++ prepare-deploy-local-dir.sh
@@ -3,9 +3,9 @@ UPDATE_SITE_DIR=releng/org.gemoc.siriusanimation.repository/target/
 DEPLOY_LOCAL_DIR=$1
 echo "Prepare deploy local dir = ${DEPLOY_LOCAL_DIR}"
 # Create nightly folder
-mkdir $DEPLOY_LOCAL_DIR 
+mkdir -p $DEPLOY_LOCAL_DIR 
 # Rename the zipped update site to nightly
-- mv $UPDATE_SITE_DIR/org.gemoc.siriusanimation.repository-*-SNAPSHOT.zip $UPDATE_SITE_DIR/org.gemoc.siriusanimation.repository-nightly.zip
+mv $UPDATE_SITE_DIR/org.gemoc.siriusanimation.repository-*-SNAPSHOT.zip $UPDATE_SITE_DIR/org.gemoc.siriusanimation.repository-nightly.zip
 # Copy update-site to deploy local dir
 cp -r $UPDATE_SITE_DIR/repository $DEPLOY_LOCAL_DIR
 cp -r $UPDATE_SITE_DIR/*.zip $DEPLOY_LOCAL_DIR
