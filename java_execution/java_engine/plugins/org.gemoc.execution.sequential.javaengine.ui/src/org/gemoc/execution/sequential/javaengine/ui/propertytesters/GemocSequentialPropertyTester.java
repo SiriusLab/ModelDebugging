d66587750f700592804839f581e84bd7ce4af72d
Didier Vojtisek <didier.vojtisek@inria.fr>
2017-01-24 09:22:39 +0100

    Fixing header copyright
    (in both java and xtend files)

--- java_execution/java_engine/plugins/org.gemoc.execution.sequential.javaengine.ui/src/org/gemoc/execution/sequential/javaengine/ui/propertytesters/GemocSequentialPropertyTester.java
+++ java_execution/java_engine/plugins/org.gemoc.execution.sequential.javaengine.ui/src/org/gemoc/execution/sequential/javaengine/ui/propertytesters/GemocSequentialPropertyTester.java
@@ -1,4 +1,3 @@
-
 /*******************************************************************************
  * Copyright (c) 2016 Inria and others.
  * All rights reserved. This program and the accompanying materials
