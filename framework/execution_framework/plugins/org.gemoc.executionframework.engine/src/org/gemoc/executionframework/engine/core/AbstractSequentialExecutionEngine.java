9b7d80262d24a5046b222fed985c66a0ea9aaaca
Didier Vojtisek <didier.vojtisek@inria.fr>
2017-01-30 14:52:19 +0100

    fixed style

--- framework/execution_framework/plugins/org.gemoc.executionframework.engine/src/org/gemoc/executionframework/engine/core/AbstractSequentialExecutionEngine.java
+++ framework/execution_framework/plugins/org.gemoc.executionframework.engine/src/org/gemoc/executionframework/engine/core/AbstractSequentialExecutionEngine.java
@@ -8,17 +8,19 @@
  * Contributors:
  *     Inria - initial API and implementation
  *******************************************************************************/
-/*  ******************************************************************************
- * C opyright (c) 2016 Inria and others.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors:
- *     Inria - initial API and implementation
- * ******************************************************************************/
 package org.gemoc.executionframework.engine.core;
+
+
+import fr.inria.diverse.k3.al.annotationprocessor.stepmanager.EventManagerRegistry;
+import fr.inria.diverse.k3.al.annotationprocessor.stepmanager.IEventManager;
+import fr.inria.diverse.trace.commons.model.trace.GenericMSE;
+import fr.inria.diverse.trace.commons.model.trace.MSE;
+import fr.inria.diverse.trace.commons.model.trace.MSEModel;
+import fr.inria.diverse.trace.commons.model.trace.MSEOccurrence;
+import fr.inria.diverse.trace.commons.model.trace.SequentialStep;
+import fr.inria.diverse.trace.commons.model.trace.Step;
+import fr.inria.diverse.trace.commons.model.trace.TraceFactory;
+import fr.inria.diverse.trace.gemoc.api.IMultiDimensionalTraceAddon;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -34,29 +36,26 @@ import org.gemoc.executionframework.engine.Activator;
 import org.gemoc.xdsmlframework.api.core.IExecutionContext;
 import org.gemoc.xdsmlframework.api.core.IExecutionEngine;
 
-import fr.inria.diverse.k3.al.annotationprocessor.stepmanager.EventManagerRegistry;
-import fr.inria.diverse.k3.al.annotationprocessor.stepmanager.IEventManager;
-import fr.inria.diverse.trace.commons.model.trace.GenericMSE;
-import fr.inria.diverse.trace.commons.model.trace.MSE;
-import fr.inria.diverse.trace.commons.model.trace.MSEModel;
-import fr.inria.diverse.trace.commons.model.trace.MSEOccurrence;
-import fr.inria.diverse.trace.commons.model.trace.SequentialStep;
-import fr.inria.diverse.trace.commons.model.trace.Step;
-import fr.inria.diverse.trace.commons.model.trace.TraceFactory;
-import fr.inria.diverse.trace.gemoc.api.IMultiDimensionalTraceAddon;
 
+/**
+ * Abstract class providing a basic implementation for sequential engines
+ *  
+ * @author Didier Vojtisek<didier.vojtisek@inria.fr>
+ *
+ */
 public abstract class AbstractSequentialExecutionEngine extends AbstractExecutionEngine implements IExecutionEngine {
 
 	private MSEModel _actionModel;
 	private IMultiDimensionalTraceAddon traceAddon;
 
-	abstract protected void executeEntryPoint();
+	protected abstract void executeEntryPoint();
 	
-	abstract protected void initializeModel();
+	
+	protected abstract void initializeModel();
 
-	abstract protected void prepareEntryPoint(IExecutionContext executionContext);
+	protected abstract void prepareEntryPoint(IExecutionContext executionContext);
 
-	abstract protected void prepareInitializeModel(IExecutionContext executionContext);
+	protected abstract void prepareInitializeModel(IExecutionContext executionContext);
 
 	@Override
 	public final void performInitialize(IExecutionContext executionContext) {
@@ -118,9 +117,9 @@ public abstract class AbstractSequentialExecutionEngine extends AbstractExecutio
 	protected final void beforeExecutionStep(Object caller, String className, String operationName, RecordingCommand rc) {
 		if (caller != null && caller instanceof EObject && editingDomain != null) {
 			// Call expected to be done from an EMF model, hence EObjects
-			EObject caller_cast = (EObject) caller;
+			EObject callerEObject = (EObject) caller;
 			// We create a step
-			Step step = createStep(caller_cast, className, operationName);
+			Step step = createStep(callerEObject, className, operationName);
 
 			manageEvents();
 
@@ -178,6 +177,16 @@ public abstract class AbstractSequentialExecutionEngine extends AbstractExecutio
 		return operation;
 	}
 
+	
+	
+	/**
+	 * Find the MSE element for the triplet caller/className/MethodName in the model of precalculated possible MSE.
+	 * If it doesn't exist yet, create one and add it to the model.
+	 * @param caller the caller object
+	 * @param className the class containing the method
+	 * @param methodName the name of the method
+	 * @return the retrieved or created MSE
+	 */
 	public final MSE findOrCreateMSE(EObject caller, String className, String methodName) {
 		EOperation operation = findOperation(caller, className, methodName);
 		// TODO Should be created/loaded before execution by analyzing the
